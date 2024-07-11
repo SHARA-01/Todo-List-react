8ec3c2c6c660dad0e8e9745bd40c2de7efb3a491
shara <chndshara@gmail.com>
2024-07-11 12:33:18 +0530

    7th fix mobile size only for desktop user

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -8,7 +8,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 function MainLayout() {
-
+  const [isMobile, setIsMobile] = useState(false);
   const [user, setUser] = useState(false)
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -17,14 +17,31 @@ function MainLayout() {
     })
   }, [user])
 
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsMobile(window.innerWidth < 1200); // Adjust the width as needed for your definition of mobile
+    };
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);    // Event listener for screen size change
+    return () => window.removeEventListener("resize", checkScreenSize);   // Clean up event listener
+  }, []);
+
   return (
     <>
-      <div>
-        <Navbar user={user} />
-        <Outlet />
-        <ToastContainer />
-        <Footer />
-      </div>
+      {isMobile ? (
+        <main className="py-8 px-4 lg:px-12">
+          <p className="text-lg lg:text-xl">
+            This page is designed for desktop users. For the best experience, please access it from a desktop or laptop device.
+          </p>
+        </main>
+      ) :
+        <div>
+          <Navbar user={user} />
+          <Outlet />
+          <ToastContainer />
+          <Footer />
+        </div>
+      }
     </>
   )
 }
